cadf97bf41191893bc7336661fb2895c8e2cb484
Anton Montagne <anton@montagne.nl>
2020-09-10 14:58:50 +0200

    Python2 version completed, ready for testing.

--- examples/Transimpedance/opAmpSpecs.m
+++
@@ -1,5 +0,0 @@
-htmlPage('Summary OpAmp requirements');
-text2html(['The table below is an imported .csv file. ' ...
-    'It gives an overview of the requirements for the operational amplifier.']);
-csv2html('opAmpSpec');
-stophtml();
