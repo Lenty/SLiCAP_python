b8aad9b82bffb5fdc8b32bf93816086945212f25
Luc Enthoven <luc.enthoven@gmail.com>
2020-10-06 13:05:59 +0200

    Added install script (beta version)

--- setup.py
+++ setup.py
@@ -5,14 +5,171 @@ Created on Tue Sep 29 09:28:49 2020
 @author: luc_e
 """
 
+import os, shutil
+from os.path import expanduser
 import setuptools
+from setuptools.command.install import install
+import platform
+import subprocess
+import re
+import in_place
+from shutil import copy
+
+INSTALLVERSION="0.9.0"
+
+class InstallWrapper(install):
+    """Provides a install wrapper for SLiCAP"""
+    _maxima_cmd = None
+    _SLiCAP_version = None
+    _library_location = None
+  
+    def run(self):
+        # Run this first so the install stops in case 
+        # these fail otherwise the Python package is
+        # successfully installed
+        self._set_maxima_command()
+        self._set_version_config()
+        self._set_lib_location()
+        self._gen_config_file()
+        # Run the standard PyPi copy
+        install.run(self)
+
+    def _set_maxima_command(self):
+        """
+        Determining the Maxima command
+        This contains two flows:
+            Windows -   Searches the drive root dir (e.g. C:\) for a maxima directory and generates the full path
+                        This path points to maxima.bat, which is found in the bin directory
+                        Manual entry of the maxima path is also possible
+            Linux -     test is the 'maxima' command is callable from the command line
+        """
+        print("Acquiring Maxima Command")
+        succes = False
+        maxInput = '1+1;'
+        if platform.system() == 'Windows':
+            self._maxima_cmd = self._find_maxima_windows()
+            
+        while not(succes):
+            result = 0
+            if platform.system() == 'Windows':
+                string = "The Maxima path found is '"+self._maxima_cmd+"', press enter to continue with this path or type the full maxima path to override this value:"
+                ret_val = input(string)
+                # print(repr(ret_val))
+                if not ret_val:
+                    print("Using found Maxima path")
+                else:
+                    if os.path.exists(ret_val):
+                        print("Using entered Maxima path")
+                        self._maxima_cmd = ret_val
+                    else:
+                        print("Entered path does not seem to exist, make sure you entered the path correctly")
+                try:
+                    result = subprocess.run([self._maxima_cmd, '--very-quiet', '-batch-string', maxInput], capture_output=True, timeout=3, text=True).stdout.split('\n')
+                    result = [i for i in result if i] # Added due to variability of trailing '\n'
+                    result = result[-1]
+                    
+                except:
+                    print("Not able to succesfully execute the maxima command, please make sure the full path points to 'maxima.bat'")
+            else:
+                try:
+                    result = subprocess.run(['maxima', '--very-quiet', '-batch-string', maxInput], capture_output=True, timeout=3, text=True).stdout.split('\n')[-1]
+                        
+                except:
+                    print("Not able to run the maxima command, verify maxima is installed by typing 'maxima' in the command line")
+
+            if int(result) == 2:
+                succes = True   
+                print("Succesfully ran Maxima command")
+
+
+    def _set_version_config(self):
+        """
+        Sets the SLiCAP version variable to be set in the config file
+        Can be appended to get the version variable from a website
+
+        Returns
+        -------
+        None.
+
+        """
+        self._SLiCAP_version = INSTALLVERSION
+        print("Slicap version:", self._SLiCAP_version)
+
+
+    def _set_lib_location(self):
+        """
+        Sets the SLiCAP library variable to be set in the config file
+        Includes copying of the default libraries
+
+        Returns
+        -------
+        None.
+
+        """
+        succes = False
+        while not(succes):
+            home = expanduser("~")
+            slicap_home = os.path.join(home, 'SLiCAP', 'lib')
+            
+            string = "Select a location for the default libraries, press enter for the default location('"+slicap_home+"'), otherwise type a full path to override this value:"
+            ret_val = input(string)
+            # print(repr(ret_val))
+            if not ret_val:
+                print("Using ", slicap_home)
+            else:
+                print("Using valid library path", ret_val)
+                slicap_home = ret_val
+            
+            try:
+                if not os.path.exists(slicap_home):
+                    os.makedirs(slicap_home)
+                for (root, dirs, files) in os.walk("lib/"):
+                    for name in files:
+                        copy(os.path.join(root, name), slicap_home)
+                self._library_location = slicap_home
+                succes = True
+            except:
+                print("could not set library location")
+
+    def _gen_config_file(self):
+        print("Generating the configuration file")
+        fileloc = os.path.join("SLiCAPtemplate.py")
+        filetarg = os.path.join("SLiCAP", "SLiCAPconfig", "SLiCAPconfig.py")
+        print(fileloc)
+        print(filetarg)
+        if os.path.isfile(fileloc):
+            print("Found template file")
+            copy(fileloc, filetarg)
+            with in_place.InPlace(filetarg) as file:
+                for line in file:
+                    line = line.replace("$VERSION", self._SLiCAP_version)
+                    line = line.replace("$LIBCOREPATH", self._library_location)
+                    line = line.replace("$MAXIMAPATH", self._maxima_cmd)
+                    # print(line)
+                    file.write(line)
+        print("Created config file")
+        
+
+    def _find_maxima_windows(self):
+        drive = os.sep.join(os.getcwd().split(os.sep)[:1])+os.sep
+        print(drive)
+        for root, dirs, files in os.walk(drive, topdown=True):
+            for name in dirs:
+                if re.match('maxima-*', name, flags=0):
+                    print("found Maxima dir")
+                    path = os.path.join(root,name, 'bin','maxima.bat')
+                    print("Maxima path set as:", path)
+                    return path
+                            
+
+      
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="SLiCAP_python", # Replace with your own username
-    version="0.9.0",
+    version=INSTALLVERSION,
     author="Anton Montagne",
     author_email="anton@montagne.nl",
     description="Symbolic Linear Circuit Analysis Program",
@@ -20,9 +177,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/Lenty/SLiCAP_python/",
     packages=setuptools.find_packages(),
-    # py_modules = ['src.SLiCAP', 'src.SLiCAPnotebook'],
-    #packages=['SLiCAP'],
-    #package_dir={'SLiCAP': 'SLiCAP'},
+    cmdclass={'install': InstallWrapper},    
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: Attribution-NonCommercial-NoDerivatives 4.0 International",
