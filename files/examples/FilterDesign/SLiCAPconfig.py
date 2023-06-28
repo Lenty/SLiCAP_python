7eff4e97c3341357f951ecd5a8391dbf903b2473
anton montagne <anton@montagne.nl>
2023-06-28 14:34:52 +0200

    Improved display of expressions, minor bug fixes

---
+++ files/examples/FilterDesign/SLiCAPconfig.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+SLiCAP module with user-defined path settings.
+
+Default values:
+
+>>> PROJECTPATH = None      # Leave it for automatic detection
+>>> # PATHS: relative to the project path
+>>> HTMLPATH    = 'html/'   # path for html output
+>>> CIRCUITPATH = 'cir/'    # path for .asc, .net, .cir, .sch files
+>>> LIBRARYPATH = 'lib/'    # path for include and library files
+>>> TXTPATH     = 'txt/'    # path for text files
+>>> CSVPATH     = 'csv/'    # path for CSV files
+>>> LATEXPATH   = 'tex/'    # path for LaTeX output
+>>> MATHMLPATH  = 'mathml/' # path for mathML output
+>>> IMGPATH     = 'img/'    # path for image files
+>>> SPHINXPATH  = 'sphinx/' # path for Sphinx output
+"""
+PROJECTPATH = None      # Leave it for automatic detection
+# PATHS: relative to the project path
+HTMLPATH    = 'html/'   # path for html output
+CIRCUITPATH = 'cir/'    # path for .asc, .net, .cir, .sch files
+LIBRARYPATH = 'lib/'    # path for include and library files
+TXTPATH     = 'txt/'    # path for text files
+CSVPATH     = 'csv/'    # path for CSV files
+LATEXPATH   = 'tex/'    # path for LaTeX output
+MATHMLPATH  = 'mathml/' # path for mathML output
+IMGPATH     = 'img/'    # path for image files
+SPHINXPATH  = 'sphinx/' # path for Sphinx output
+
+# Project information
+PROJECT    = '4-th order Linkwitz-Riley Filter'
+AUTHOR     = 'anton'
+CREATED    = '2023-06-12 11:38:39.115081'
+LASTUPDATE = '2023-06-12 11:38:39.115081'
---
+++ files/examples/MOSparams/MOSparams.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 12 11:40:21 2023
+
+@author: anton
+"""
+
+from SLiCAP import *
+
+prj = initProject('Plotting of MOS characteristics') 
+
+import EKVplotsN_V
+import EKVplotsN
+import EKVplotsP_V
+import EKVplotsP
