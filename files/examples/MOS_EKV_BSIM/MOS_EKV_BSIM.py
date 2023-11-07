80bfa3b68ff0ffbd1d7a178608ac7c08e45e75fc
anton montagne <anton@montagne.nl>
2023-11-07 10:37:57 +0100

    Update example MOS_EKV_BSIM.

--- files/examples/MOS_EKV_BSIM/MOS_EKV_BSIM.py
+++ files/examples/MOS_EKV_BSIM/MOS_EKV_BSIM.py
@@ -14,20 +14,22 @@ prj = initProject("MOS_EKV_BSIM")
 
 # Define library, device, geometry and operating point
 LIB    = 'lib/log018.l TT'
-DEV    = 'pch'
-W      = 2.2e-6
-L      = 1.8e-6
+DEV    = 'nch'
+W      = .22e-6
+L      = .18e-6
 M      = 1
 f      = 1E6
 refDes = 'M1'
+
+# Define number of points and the absolute value of the gate step voltage
 Npts   = 90
-Vdiff  = 0.01
+Vdiff  = 0.02
 
 # Create the device
 mn = MOS(refDes, LIB, DEV)
 
 if DEV == "nch":
-    VD   = 0.9
+    VD   = 1.8
     VS   = 0
     VB   = 0
     VG   = 0
@@ -40,7 +42,8 @@ elif DEV == "pch":
     VG   = 1.8
     step = [VS, Npts, -Vdiff]
 
-#step=None # Uncomment if you want single-point info
+# Uncomment the following line to obtain single operating point information
+#step=None
 
 mn.getOPvg(W, L, M, VG, VD, VS, VB, f, step)
 
@@ -71,10 +74,10 @@ else:
     derivedParams['fT'] = mn.params['ggd']/(2*np.pi*mn.params['cgg'])
     derivedParams['gm/Id\t'] = mn.params['ggd']/mn.params['i(ids)']
 
-    # Get SLiCAP EKV model data
-
+    # Define SLiCAP circuit with current-controlled EKV model devices (only valid in linear region)
     if DEV == "nch":
         cirText = "EKV model\n.lib CMOS18.lib\nX1 D G S 0 CMOS18N W={W} L={L} ID={I_D} \n.param I_D=0 W=%s L=%s\n.end\n"%(W, L)
+
     elif DEV == "pch":
         cirText = "EKV model\n.lib CMOS18.lib\nX1 D G S 0 CMOS18P W={W} L={L} ID={-I_D} \n.param I_D=0 W=%s L=%s\n.end\n"%(W, L)
 
@@ -88,8 +91,59 @@ else:
     i1.defPar('L', L)
     i1.delPar('I_D')
 
-    ID = sp.Symbol('I_D')
+    # Store and redefine SLiCAP EKV library model data
+    if DEV == "nch":
+        # Store CMOS18 N channel process data from library:
+        TOX    = i1.getParValue('TOX_N18')    # oxide thickness [m]
+        Vth    = i1.getParValue('Vth_N18')    # threshold voltage [V]
+        N_s    = i1.getParValue('N_s_N18')    # substrate factor [-]
+        Theta  = i1.getParValue('Theta_N18')  # vertical field mobility reduction coefficient [1/V]
+        E_CRIT = i1.getParValue('E_CRIT_N18') # lateral field strength for velocity saturation [V/m]
+        u_0    = i1.getParValue('u_0_N18')    # zero field carrier mobility [m^2/V/s]
+        CGBO   = i1.getParValue('CGBO_N18')   # gate-bulk overlap capacitance [F/m]
+        CGSO   = i1.getParValue('CGSO_N18')   # gate-source and gate-drain overlap capacitance [F/m]
+        CJB0   = i1.getParValue('CJB0_N18')   # source/bulk drain/bulk capacitance [F/m^2]
+        LDS    = i1.getParValue('LDS_N18')    # length of drain and source [m]
+        VAL    = i1.getParValue('VAL_N18')    # Early voltage per unit of length [V/m]
+        # You can redefine these process parameters by overwriting the defaults
+        i1.defPar('TOX_N18', TOX)             # oxide thickness [m]
+        i1.defPar('Vth_N18', Vth)             # threshold voltage [V]
+        i1.defPar('N_s_N18', N_s)             # substrate factor [-]
+        i1.defPar('Theta_N18', Theta)         # vertical field mobility reduction coefficient [1/V]
+        i1.defPar('E_CRIT_N18', E_CRIT)       # lateral field strength for velocity saturation [V/m]
+        i1.defPar('u_0_N18', u_0)             # zero field carrier mobility [m^2/V/s]
+        i1.defPar('CGBO_N18', CGBO)           # gate-bulk overlap capacitance [F/m]
+        i1.defPar('CGSO_N18', CGSO)           # gate-source and gate-drain overlap capacitance [F/m]
+        i1.defPar('CJB0_N18', CJB0)           # source/bulk drain/bulk capacitance [F/m^2]
+        i1.defPar('LDS_N18', LDS)             # length of drain and source [m]
+        i1.defPar('VAL_N18', VAL)             # Early voltage per unit of length [V/m]
+    elif DEV == "pch":
+        # Store CMOS18 P channel process data from library:
+        TOX    = i1.getParValue('TOX_P18')    # oxide thickness [m]
+        Vth    = i1.getParValue('Vth_P18')    # threshold voltage [V]
+        N_s    = i1.getParValue('N_s_P18')    # substrate factor [-]
+        Theta  = i1.getParValue('Theta_P18')  # vertical field mobility reduction coefficient [1/V]
+        E_CRIT = i1.getParValue('E_CRIT_P18') # lateral field strength for velocity saturation [V/m]
+        u_0    = i1.getParValue('u_0_P18')    # zero field carrier mobility [m^2/V/s]
+        CGBO   = i1.getParValue('CGBO_P18')   # gate-bulk overlap capacitance [F/m]
+        CGSO   = i1.getParValue('CGSO_P18')   # gate-source and gate-drain overlap capacitance [F/m]
+        CJB0   = i1.getParValue('CJB0_P18')   # source/bulk drain/bulk capacitance [F/m^2]
+        LDS    = i1.getParValue('LDS_P18')    # length of drain and source [m]
+        VAL    = i1.getParValue('VAL_P18')    # Early voltage per unit of length [V/m]
+        # You can redefine these process parameters by overwriting the defaults
+        i1.defPar('TOX_P18', TOX)             # oxide thickness [m]
+        i1.defPar('Vth_P18', Vth)             # threshold voltage [V]
+        i1.defPar('N_s_P18', N_s)             # substrate factor [-]
+        i1.defPar('Theta_P18', Theta)         # vertical field mobility reduction coefficient [1/V]
+        i1.defPar('E_CRIT_P18', E_CRIT)       # lateral field strength for velocity saturation [V/m]
+        i1.defPar('u_0_P18', u_0)             # zero field carrier mobility [m^2/V/s]
+        i1.defPar('CGBO_P18', CGBO)           # gate-bulk overlap capacitance [F/m]
+        i1.defPar('CGSO_P18', CGSO)           # gate-source and gate-drain overlap capacitance [F/m]
+        i1.defPar('CJB0_P18', CJB0)           # source/bulk drain/bulk capacitance [F/m^2]
+        i1.defPar('LDS_P18', LDS)             # length of drain and source [m]
+        i1.defPar('VAL_P18', VAL)             # Early voltage per unit of length [V/m]
 
+    ID = sp.Symbol('I_D')
     IDS = mn.params['i(ids)']
 
     GM                = sp.lambdify(ID, i1.getParValue('g_m_X1'))
