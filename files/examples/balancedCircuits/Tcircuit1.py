766c071a716ce422b42420f3534b5e201a70951a
anton montagne <anton@montagne.nl>
2023-11-26 12:27:23 +0100

    Update Example balanced noise

--- files/examples/balancedCircuits/Tcircuit1.py
+++ files/examples/balancedCircuits/Tcircuit1.py
@@ -42,7 +42,7 @@ matrices2html(result)
 head2html("DM-CM matrix equation")
 head3html("Gain type: vi")
 i1.setPairExt(['P', 'N'])
-i1.setConvType('all') 
+i1.setConvType('all')
 i1.setGainType("vi")
 i1.setDataType("matrix")
 result = i1.execute()
@@ -56,7 +56,7 @@ matrices2html(result)
 
 head2html("DM matrix equation")
 head3html("Gain type: vi")
-i1.setConvType('dd') 
+i1.setConvType('dd')
 i1.setGainType("vi")
 i1.setDataType("matrix")
 result = i1.execute()
@@ -71,7 +71,7 @@ eqn2html("Z_dm", result.laplace)
 
 head2html("CM matrix equation")
 head3html("Gain type: vi")
-i1.setConvType('cc') 
+i1.setConvType('cc')
 i1.setGainType("vi")
 i1.setDataType("matrix")
 result = i1.execute()
@@ -96,18 +96,18 @@ DMonoise = noiseResult.onoise
 DMinoise = noiseResult.inoise
 
 htmlPage("DM Noise analysis")
-i1.setConvType('dd') 
+i1.setConvType('dd')
 i1.setDetector('V_in_D')
 i1.setGainType("vi")
 i1.setDataType("noise")
 noiseResult = i1.execute()
 matrices2html(noiseResult)
 noise2html(noiseResult)
-if noiseResult.onoise == DMonoise:
+if sp.N(noiseResult.onoise - DMonoise) == sp.N(0):
     print('DMonoise OK!')
 else:
     print('DMonoise NOT OK!')
-if noiseResult.onoise == DMonoise:
+if sp.N(noiseResult.onoise - DMonoise) == sp.N(0):
     print('DMinoise OK!')
 else:
     print('DMinoise NOT OK!')
@@ -116,7 +116,7 @@ htmlPage("CM Noise analysis")
 i1.setDetector('V_in_C')
 i1.setGainType("vi")
 i1.setDataType("noise")
-i1.setConvType('cc') 
+i1.setConvType('cc')
 noiseResult = i1.execute()
 matrices2html(noiseResult)
 noise2html(noiseResult)
